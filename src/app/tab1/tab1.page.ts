3e82e4a5d722a05a0093ba0c18ae996a82e26f27
barbaragasparoto <barbaragasparotofigueiredo@gmail.com>
2023-03-09 12:26:39 -0300

    09/03

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,9 +1,18 @@
+import {animate, state, style, transition, trigger}  from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  animations: [
+    trigger('rotatedState' , [
+          state('defaulf', style({transform: 'rotateY(0)'})),
+          state('rotated', style ({ transform: 'rotateY(-360deg)'})),
+          transition('rotated => default', animate('2000ms ease-out')),
+          transition('default => rotated', animate('2000ms ease-in'))
+    ])
+  ]
 })
 export class Tab1Page {
 
@@ -12,10 +21,18 @@ export class Tab1Page {
   logo = 'assets/logo.png';
   image = this.logo;
   info = 'Clique no botão para jogar!';
+  state = 'default';
 
   constructor() {}
 
   jogarMoeda(){
+    this.info = 'Girando...';
+    this.image = this.logo;
+    this.state = this.state === 'default' ? 'rotated' : 'default';
+
+
+    setTimeout(() => {
+
     if(Math.random() < 0.5) {
       this.image = this.cara;
       this.info = "Cara!"
@@ -24,6 +41,7 @@ export class Tab1Page {
       this.image = this.coroa;
       this.info = "Coroa!"
     }
-  }
 
+    }, 2000);
+  }
 }
