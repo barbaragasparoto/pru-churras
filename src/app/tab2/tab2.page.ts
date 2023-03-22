c08d2b7982a787b505da66ec6a0b54988aff1f1b
barbaragasparoto <>
2023-03-22 08:59:12 -0300

    tab2.page

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,82 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+
+valendo=1;
+pontuacaoA=0;
+pontuacaoB=0;
+vitoriaA=0;
+vitoriaB=0;
+
+
+constructor() {}
+
+aumentarValor(valor: number){
+this.valendo=valor
+}
+
+aumentarTento(time: number){
+if(time===1){
+this.pontuacaoA += this.valendo;
+this.checarVitoria();
+this.valendo = 1;
+}
+
+if(time===2){
+this.pontuacaoB += this.valendo;
+this.checarVitoria();
+this.valendo = 1;
+}
 
 }
+
+diminuirTento(time: number){
+if(time === 1 && this.pontuacaoA > 0){
+  this.pontuacaoA -= this.valendo;
+
+  if(this.pontuacaoA <= 0) {
+    this.pontuacaoA = 0
+  }
+
+  this.valendo = 1
+  }
+
+if(time === 2 && this.pontuacaoB > 0){
+    this.pontuacaoB -= this.valendo;
+
+    if(this.pontuacaoB <= 0) {
+      this.pontuacaoB = 0
+    }
+
+    this.valendo = 1
+    }
+}
+
+
+checarVitoria(){
+  if(this.pontuacaoA>=12){
+    this.pontuacaoA = 0
+    this.pontuacaoB = 0
+    this.vitoriaA++;
+  }
+
+  if(this.pontuacaoB>=12){
+    this.pontuacaoB = 0
+    this.pontuacaoA = 0
+    this.vitoriaB++;
+  }
+
+}
+
+limpar(){
+  this.pontuacaoA = 0
+  this.pontuacaoB = 0
+  this.valendo = 1
+  this.vitoriaA = 0
+  this.vitoriaB = 0
+
+}
+
+}
+
+
